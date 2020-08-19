92c6dc80333d834557e1a368599ab534ab3ea110
Murilo Lombardi <murilomarinato@ucl.br>
2020-08-19 15:44:12 +0000

    Exercício 2

--- main.cs
+++ main.cs
@@ -6,7 +6,7 @@ Colocar como entrega o link do repositório no Github.*/
 
 
 //Excercício 1
-using System;
+/*using System;
 
 class MainClass {
   public static float Operacoes(float n1, float n2){ 
@@ -18,5 +18,24 @@ class MainClass {
     n1=float.Parse(Console.ReadLine());
     n2=float.Parse(Console.ReadLine());
     Console.WriteLine("A soma dos dois números é: {0}",Operacoes(n1,n2));
+  }
+}*/
+
+
+//Excercício 2
+using System;
+
+class MainClass {
+  public static float Operacoes(float n1, float n2, float n3, float n4){ 
+  return((n1+n2+n3+n4)/4);
+  }
+  public static void Main (string[] args) {
+    float n1, n2, n3, n4;
+    Console.WriteLine ("Digite as quatros notas: ");
+    n1=float.Parse(Console.ReadLine());
+    n2=float.Parse(Console.ReadLine());
+    n3=float.Parse(Console.ReadLine());
+    n4=float.Parse(Console.ReadLine());
+    Console.WriteLine("A média das notas é: {0}",Operacoes(n1,n2,n3,n4));
   }
 }
